e7db727cf2d6308011a02e55e4aa838cf0e479ad
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-09-14 10:08:59 +0800

    first commit

---
+++ buyDevice.js
@@ -0,0 +1,29 @@
+var amount = 904000;
+if (amount > 80000) {
+    console.log('Its a mac users price');
+}
+else if (amount > 60000) {
+    console.log("its a Gaming PC");
+}
+else if (amount > 40000) {
+    console.log("its a Lenove xp");
+}
+else if (amount > 20000) {
+    console.log("low budget hp laptop")
+}
+else {
+    console.log("i have not insufficient Money so i will be buy a phone")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ device.js
@@ -0,0 +1,24 @@
+// Mizba Uddin Tareq
+
+var myMoney = 17000;
+
+if (myMoney > 80000) {
+
+    console.log("I will buy MAC");
+    
+} else if (myMoney > 60000) {
+
+    console.log("I will buy gaming laptop");
+    
+} else if (myMoney > 40000) {
+
+    console.log("I will buy lenovo yoga laptop");
+    
+} else if (myMoney > 20000) {
+
+    console.log("I will buy old laptop");
+    
+} else {
+    console.log("I will buy a Xiaomi phone");
+    
+}
